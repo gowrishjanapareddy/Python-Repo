ff72811757152d8bf7098d50c541795f6996b336
gowrishjanapareddy <147791808+gowrishjanapareddy@users.noreply.github.com>
2024-10-16 16:06:44 +0530

    Uploading code: Find Second Number.py

---
+++ Find Second Number.py
@@ -0,0 +1,11 @@
+import java.util.*;
+public class b{
+    public static void main(String[] args){
+      int a,b,c;
+      Scanner sc =new Scanner(System.in);
+       a=sc.nextInt();
+       b=sc.nextInt();
+       c=2*a-b;
+       System.out.println(c);  
+    }
+}
